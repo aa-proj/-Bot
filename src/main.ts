15da56b272a5e73e793cdba85d3cbd118a8c3095
rassi0429 <taitokokoa+rassi@gmail.com>
2022-10-28 21:32:22 +0900

    feat: アプデ

--- src/main.ts
+++ src/main.ts
@@ -1,204 +1,179 @@
-import {Client, Emoji, Message, TextChannel} from "discord.js";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  Client,
+  IntentsBitField,
+  ButtonStyle,
+  Message,
+  TextChannel
+} from "discord.js";
 import {Connection, ConnectionOptions, createConnection} from "typeorm";
 import {User} from "./eneity/User";
 import {Furo} from "./eneity/furo";
-import axios from "axios";
+import {getTimeFromMills, giveAAPoint} from "./util";
 
 
-const bathReaction = "<:nyuyoku:885703314417807420>";
+// const bathReaction = "<:nyuyoku:885703314417807420>";
+const bathReactionId = "885703314417807420"
 const dailyBotChannel = "803321643803213834";
 const generalChannel = "606109479003750442";
 const guildID = "606109479003750440";
 
 // Discordクライアント
-const client = new Client();
+const client = new Client(
+  {
+    intents:
+      [IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildMessageReactions
+      ]
+  });
 
-// リアクションされたときのインターフェース
-interface IReaction {
-    user_id: string; // Discord uid
-    message_id: string; // Message id
-    emoji: Emoji; // EmojiResolvable
-    channel_id: string; // ChannelID
-    guild_id: string; // GuildID
-}
 
 // TypeORMのオプション
 const options: ConnectionOptions = {
-    type: "sqlite",
-    database: "./db/db.sqlite3",
-    entities: [User, Furo],
-    synchronize: false,
+  type: "sqlite",
+  database: "./db/db.sqlite3",
+  entities: [User, Furo],
+  synchronize: false,
 };
 
 // TypeORMのコネクション 使う前にnullチェックが必要
 let connection: Connection | null = null;
 
 async function connectDB() {
-    connection = await createConnection(options);
-    await connection.query("PRAGMA foreign_keys=OFF");
-    await connection.synchronize();
-    await connection.query("PRAGMA foreign_keys=ON");
-    console.log("DB Connected!!")
+  connection = await createConnection(options);
+  await connection.query("PRAGMA foreign_keys=OFF");
+  await connection.synchronize();
+  await connection.query("PRAGMA foreign_keys=ON");
+  console.log("DB Connected!!")
 }
 
 // コネクションする
 connectDB();
 
-// rawイベントを取得 リアクションのイベントを発火させてる
-client.on("raw", (reaction) => {
-    switch (reaction.t) {
-        case "MESSAGE_REACTION_ADD":
-            msgReactionAdd(reaction.d);
-            break;
-    }
-});
+// // rawイベントを取得 リアクションのイベントを発火させてる
+// client.on("raw", (reaction) => {
+//     switch (reaction.t) {
+//         case "MESSAGE_REACTION_ADD":
+//             msgReactionAdd(reaction.d);
+//             break;
+//     }
+// });
 
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isButton() || !interaction.guildId) {
+    return;
+  }
+  const g = client.guilds.cache.get(interaction.guildId);
+  const general: TextChannel = <TextChannel>await g?.channels.fetch(generalChannel);
+  const messageUser = interaction.user;
+  const messageUserId = interaction.user.id;
+  const command = interaction.customId;
+  if (messageUser.bot) return;
 
-// メッセージにリアクションがあったとき
-async function msgReactionAdd(reaction: IReaction) {
-    let g = client.guilds.cache.get(reaction.guild_id);
-    const general = <TextChannel>g?.channels.cache.get(generalChannel); // TODO
+  const userRepository = connection?.getRepository(User);
+  const furoRepository = connection?.getRepository(Furo);
+  const user = await userRepository?.findOne({discordId: messageUserId});
+  const furoUser = await furoRepository?.findOne({where: {user: user}, order: {time: "DESC"}});
+  const furoTime = furoUser?.time
 
-    if (g?.member(reaction.user_id)?.user.bot) return;
-    const userRepository = connection?.getRepository(User);
-    const furoRepository = connection?.getRepository(Furo);
-    const user = await userRepository?.findOne({ discordId: reaction.user_id });
-    const furoUser = await furoRepository?.findOne({where:{user: user},order:{time:"DESC"}});
-    const furoTime = furoUser?.time
-
-    if (!user) {
-        // Userが未登録だった時
-        const newUser = userRepository?.create({
-            discordId: reaction.user_id,
+  if (!user) {
+    // Userが未登録だった時
+    const newUser = userRepository?.create({
+      discordId: messageUserId,
+    });
+    await userRepository?.save(<User>newUser);
+  }
+  {
+    switch (command) {
+      case "Furo":
+        const furoTransaction = furoRepository?.create({
+          time: new Date(),
+          user: user,
         });
-        await userRepository?.save(<User>newUser);
-    }
-    {
-        switch (reaction.emoji.name) {
-            case "nyuyoku":
-                if (true) {
-                    const furoTransaction = furoRepository?.create({
-                        time: new Date(),
-                        user: user,
-                    });
-                    furoRepository?.save(<Furo>furoTransaction);
-                    if(!furoTime) {
-                        await general.send(
-                            getNamefromID(reaction.user_id) +
-                            "は初めてお風呂に入りました"
-                        );
-                    } else {
-                        const aap = calcAAPoint((new Date().getTime()) - (new Date(furoTime).getTime()))
-                        if(aap !== 0) {
-                            await giveAAPoint(reaction.user_id, aap)
-                        }
-                        await general.send(
-                            getNamefromID(reaction.user_id) +
-                            "は" +
-                            getTimeFromMills((new Date().getTime()) - (new Date(furoTime).getTime()))
-                            + "ぶりにお風呂に入りました\n"
-                            + `${aap}ああP付与されました`
-                        );
-                    }
-                }
-                initMsg();
-                break;
+        furoRepository?.save(<Furo>furoTransaction);
+        if (!furoTime) {
+          await general.send(
+            getNameFromID(messageUserId) +
+            "は初めてお風呂に入りました"
+          );
+        } else {
+          const aap = calcAAPoint((new Date().getTime()) - (new Date(furoTime).getTime()))
+          if (aap !== 0) {
+            await giveAAPoint(messageUserId, aap)
+          }
+          await general.send(
+            getNameFromID(messageUserId) +
+            "は" +
+            getTimeFromMills((new Date().getTime()) - (new Date(furoTime).getTime()))
+            + "ぶりにお風呂に入りました\n"
+            + `${aap}ああP付与されました`
+          );
         }
+        break;
     }
-}
+  }
+  await interaction.reply({content: "OK", ephemeral: true});
+})
 
 function calcAAPoint(time: number): number {
-    const hour = Math.ceil(time / (1000 * 60 * 60))
-    if(0 <= hour && hour < 6) {
-        return 0
-    } else if(6 <= hour && hour < 18) {
-        return 6
-    } else if(18 <= hour && hour < 36) {
-        return 12
-    } else {
-        return 0
-    }
+  const hour = Math.ceil(time / (1000 * 60 * 60))
+  if (0 <= hour && hour < 6) {
+    return 0
+  } else if (6 <= hour && hour < 18) {
+    return 6
+  } else if (18 <= hour && hour < 36) {
+    return 12
+  } else {
+    return 0
+  }
 }
-
-async function giveAAPoint(id: string, point: number) {
-    await generateMoneyById(id, point)
-}
-
-async function sendMoneyById(fromId: number | string, toId: number | string, amount: number, memo: string = ""): Promise<boolean> {
-    const sendBody = {fromId: fromId.toString(), toId: toId.toString(), amount, memo}
-    // console.log(sendBody)
-    const {data} = await axios.post( "https://bank.ahoaho.jp/", sendBody)
-    if (!data.success) throw new Error(data.message)
-    return true
-}
-
-async function generateMoneyById(toId: number | string, amount: number): Promise<boolean> {
-    const result = await sendMoneyById("885834421771567125", toId, amount)
-    return result
-}
-
 
 // メッセージが来たとき
-client.on("message", async (msg) => {
-    if (msg.author.bot) return;
-    if (!msg.content.startsWith("/bath")) return;
-    // 引数をパース
-    const args = msg.content.replace(/　+/g, " ").slice(5).trim().split(/ + /);
-    switch (args[0]) {
-        case "init":
-            await initBotMessage(msg);
-            break;
-    }
+client.on("messageCreate", async (msg) => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith("/bath")) return;
+  // 引数をパース
+  const args = msg.content.replace(/　+/g, " ").slice(5).trim().split(/ + /);
+  switch (args[0]) {
+    case "init":
+      await initBotMessage(msg);
+      break;
+  }
 });
 
 // discord uid からニックネームをとってくるメソッド
-function getNamefromID(id: string) {
-    let g = client.guilds.cache.get(guildID);
-    let nickName = g?.member(id)?.nickname?.replace("@", "＠");
-    if (!nickName) nickName = g?.member(id)?.displayName;
-    return nickName;
+function getNameFromID(id: string) {
+  let g = client.guilds.cache.get(guildID);
+  let nickName = g?.members.cache.get(id)?.nickname?.replace("@", "＠");
+  if (!nickName) nickName = g?.members.cache.get(id)?.displayName;
+  return nickName;
 }
 
 // 今リアクションを待機してるメッセージ
 let nowMessage: Message | null = null;
 
 async function initBotMessage(msg: Message) {
-    const msgRes = await (<TextChannel>client.channels.cache.get(dailyBotChannel)).send(
-        "お風呂に入ったらリアクションしてください"
+
+  const channel: TextChannel = (await client.channels.fetch(dailyBotChannel)) as TextChannel;
+  const action = new ActionRowBuilder()
+    .addComponents([
+        new ButtonBuilder()
+          .setCustomId("Furo")
+          .setLabel("風呂")
+          .setStyle(ButtonStyle.Success)
+          .setEmoji(bathReactionId)
+      ]
     );
-    await msgRes.react(bathReaction);
-    nowMessage = msgRes;
-    await msg.delete();
-}
-
-
-function initMsg() {
-    if (!nowMessage) {
-        console.log("not initialized");
-        return;
-    }
-    nowMessage.reactions.removeAll().then(() => {
-        nowMessage?.react(bathReaction);
-    });
-}
-
-
-
-// ミリ秒を x時間x分x秒にするやつ いらない単位は消える
-function getTimeFromMills(m: number) {
-    let byo: number = Math.floor(m / 1000) % 60;
-    let hun: number = Math.floor(m / 60000) % 60;
-    let ji: number = Math.floor(m / 3600000);
-    let result = "";
-    if (ji != 0) result += ji + "時間 ";
-    if (hun != 0) result += hun + "分 ";
-    if (byo != 0) result += byo + "秒";
-    return result;
+  // @ts-ignore
+  await channel.send({content: "お風呂に入ったらリアクションしてください", components: [action]});
 }
 
 // DiscordBotがいい感じになったとき
 client.on("ready", () => {
-    console.log("Discord Bot Ready");
+  console.log("Discord Bot Ready");
 });
 
 client.login(process.env.FURO_TOKEN);
